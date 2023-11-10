1063f8369a49dae8a1ef3cb62351de330b27bfe7
Simon Ramstedt <simonramstedt@gmail.com>
2023-11-10 10:49:42 +0100

    upd_open_clip.py

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -6,7 +6,7 @@ deepspeed llava/train/train_mem.py \
     --version plain \
     --data_path ./playground/data/llava_v1_5_mix665k.json \
     --image_folder ./playground/data \
-    --vision_tower ./hf/siglip \
+    --vision_tower ./hf/siglip/models--timm--ViT-B-16-SigLIP \
     --pretrain_mm_mlp_adapter ./checkpoints/vicuna-7b_siglip_pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -6,7 +6,7 @@ deepspeed llava/train/train_mem.py \
     --version plain \
     --data_path ./datasets/blip/blip_laion_cc_sbu_558k.json \
     --image_folder ./datasets/blip/images \
-    --vision_tower ./hf/siglip \
+    --vision_tower ./hf/siglip/models--timm--ViT-B-16-SigLIP \
     --mm_projector_type mlp2x_gelu \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
