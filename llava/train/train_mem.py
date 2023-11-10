fa0a669a5430422a6104e68ce84b321990001c99
Simon Ramstedt <simonramstedt@gmail.com>
2023-11-10 10:28:52 +0100

    upd_train_mem.py

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('.')
 import os
 
-os.environ['WANDB_DIR'] = './wandb'
+os.environ['WANDB_DIR'] = '.'
 # os.environ['MIOPEN_USER_DB_PATH'] = '/lustre/orion/csc538/scratch/lfsm/miopoen'
 # os.environ['TRANSFORMERS_CACHE'] = '/lustre/orion/csc538/proj-shared/hf_cache'
 # os.environ['WANDB_MODE'] = 'offline'
@@ -13,9 +13,9 @@ os.environ['TRANSFORMERS_OFFLINE'] = '1'
 os.environ['HF_DATASETS_OFFLINE'] = '1'
 # os.environ['MIOPEN_CUSTOM_CACHE_DIR'] = os.environ['MIOPEN_USER_DB_PATH']
 
-from llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+# from llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
-replace_llama_attn_with_flash_attn()
+# replace_llama_attn_with_flash_attn()
 
 from llava.train.train import train
 
