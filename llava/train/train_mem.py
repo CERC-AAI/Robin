48122184a2f308bd4ba67b5bc701f939bf9ba97c
Simon Ramstedt <simonramstedt@gmail.com>
2023-11-10 11:10:27 +0100

    upd_vision_tower

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -2,15 +2,15 @@
 # Adopted from tatsu-lab@stanford_alpaca. Below is the original copyright:
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 import sys
-sys.path.append('.')
+sys.path.append('.')  # TODO: is this really necessary?
 import os
 
-os.environ['WANDB_DIR'] = '.'
+os.environ['WANDB_DIR'] = '.'  # TODO: is this really necessary?
 # os.environ['MIOPEN_USER_DB_PATH'] = '/lustre/orion/csc538/scratch/lfsm/miopoen'
 # os.environ['TRANSFORMERS_CACHE'] = '/lustre/orion/csc538/proj-shared/hf_cache'
 # os.environ['WANDB_MODE'] = 'offline'
-os.environ['TRANSFORMERS_OFFLINE'] = '1'
-os.environ['HF_DATASETS_OFFLINE'] = '1'
+# os.environ['TRANSFORMERS_OFFLINE'] = '1'
+# os.environ['HF_DATASETS_OFFLINE'] = '1'
 # os.environ['MIOPEN_CUSTOM_CACHE_DIR'] = os.environ['MIOPEN_USER_DB_PATH']
 
 # from llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
