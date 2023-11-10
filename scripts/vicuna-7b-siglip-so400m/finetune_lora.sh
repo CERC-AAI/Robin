2a15cf699398faeccdc45b73785b09f2a4d14f1a
Simon Ramstedt <simonramstedt@gmail.com>
2023-11-10 14:28:49 +0000

    upd_finetune_lora.sh

--- scripts/vicuna-7b-siglip-so400m/finetune_lora.sh
+++ scripts/vicuna-7b-siglip-so400m/finetune_lora.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 deepspeed llava/train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path ./hf/vicuna-7b \
     --version v1 \
@@ -15,16 +16,16 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/vicuna-7b-siglip-so400m-finetune \
+    --output_dir ./checkpoints/vicuna-7b-siglip-so400m-finetune-lora \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 100 \
     --save_total_limit 1 \
-    --learning_rate 2e-5 \
+    --learning_rate 2e-4 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
