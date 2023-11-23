da169af9bb70870a48399f5f1afe3ecfb7bcfc04
rogerale <alexis.roger@.umontreal.ca>
2023-11-23 15:01:41 -0500

    fixed mistral-7b-siglip-so400m

--- scripts/mistral-7b-siglip-so400m/cli.sh
+++ scripts/mistral-7b-siglip-so400m/cli.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# NOT WORKING
-
 python -m llava.serve.cli \
     --model-path agi-collective/mistral-7b-siglip-so400m-finetune-lora \
-    --model-base teknium/OpenHermes-2.5-Mistral-7B \
+    --model-base mistralai/Mistral-7B-v0.1 \
     --image-file "https://llava-vl.github.io/static/images/view.jpg" 
