4bf11e526275c2db4efc27fa3d79f4a1f32d2ef8
Alexis Roger <alexisroger@dtn35.ccs.ornl.gov>
2023-12-29 11:14:31 -0500

    improved relaunch script

--- scripts/frontier/relaunch_batch.py
+++ scripts/frontier/relaunch_batch.py
@@ -1,7 +1,14 @@
 jobids = [
-    1552987,
-    1553019,
-    1552391
+1553848,
+1553849,
+1553850,
+1553851,
+1553852,
+1553853,
+1553854,
+1553855,
+1553856,
+1553857
 ]
 
 import os
@@ -28,31 +35,34 @@ echo "$outdir"
 cp /lustre/orion/csc538/scratch/$(whoami)/job_logs/*-{jobid}.* $outdir/
     """)
 
-while len(jobids) > 0:
-    new_jobids = jobids.copy()
-    for jobid in jobids:
-        status = run(f'sacct -j {jobid} --format=JobID,state | grep "{jobid} " | sed "s/[0-9]* //g"')[0]
-        print(status)
-        match status:
-            case "PENDING":
-                pass
-            case "RUNNING":
-                pass
-            case "COMPLETED":
-                print(f"Job {jobid} is completed")
-                new_jobids.remove(jobid)
-            case "FAILED":
-                print(f"Job {jobid} has failed")
-                new_jobids.remove(jobid)
-            case "TIMEOUT":
-                cp_logs(jobid)
-                new_id = run(f'echo "$(sacct -j {jobid} -B | tail -n +3 | sbatch)" | grep -oG "[0-9]*"')
-                print(f"Job {jobid} has timeout, relaunching as {new_id}")
-                new_jobids.remove(jobid)
-                new_jobids.append(int(new_id))
-            case _:
-                print(f"Unknown job status: {jobid} {status}")
+if __name__ == '__main__':
+    while len(jobids) > 0:
+        new_jobids = jobids.copy()
+        for jobid in jobids:
+            status = run(f'sacct -j {jobid} --format=JobID,state | grep "{jobid} " | sed "s/[0-9]* //g"')[0]
+            # print(jobid, status, end=', ')
+            match status:
+                case "PENDING":
+                    print(f"Job {jobid} is pending", end=', ')
+                case "RUNNING":
+                    print(f"Job {jobid} is running", end=', ')
+                case "COMPLETED":
+                    print(f"Job {jobid} is completed", end=', ')
+                    new_jobids.remove(jobid)
+                case "FAILED":
+                    print(f"Job {jobid} has failed", end=', ')
+                    new_jobids.remove(jobid)
+                case "TIMEOUT":
+                    cp_logs(jobid)
+                    new_id = run(f'echo "$(sacct -j {jobid} -B | tail -n +3 | sbatch)" | grep -oG "[0-9]*"')[0]
+                    print()
+                    print(f"Job {jobid} has timeout, relaunching as {new_id}")
+                    new_jobids.remove(jobid)
+                    new_jobids.append(int(new_id))
+                case _:
+                    print(f"Unknown job status: {jobid} {status}", end=', ')
 
-    jobids = new_jobids
-    sleep(60)
+        jobids = new_jobids
+        print()
+        sleep(60)
                 
