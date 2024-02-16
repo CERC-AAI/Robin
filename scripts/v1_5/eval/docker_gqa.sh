9cab28d556bb811964e1dd27570941a48fcc1818
Alexis-BX <rogeralexis@icewindale.dms.umontreal.ca>
2024-02-16 17:38:52 -0500

    rewied eval file structure

---
+++ scripts/v1_5/eval/docker_gqa.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MODEL=$1
+BASE=$2
+#$3=""
+
+SPLIT="llava_gqa_testdev_balanced"
+GQADIR="/app/playground/data/eval/gqa"
+
+python /app/robin/robin/eval/model_vqa_loader.py \
+    --model-path $MODEL \
+    --question-file $GQADIR/data/$SPLIT.jsonl \
+    --image-folder $GQADIR/data/images \
+    --answers-file $GQADIR/answers/$3/$SPLIT/$MODEL/answers.jsonl \
+    --temperature 0 \
+    --model-base $BASE \
+    --conv-mode vicuna_v1
+
+
+output_file=$GQADIR/answers/$3/$SPLIT/$MODEL/answers.jsonl
+
+python /app/robin/scripts/convert_gqa_for_eval.py --src $output_file --dst $GQADIR/data/testdev_balanced_predictions.json
+
+cd $GQADIR
+python eval.py --tier data/testdev_balanced
--- scripts/v1_5/eval/docker_sqa.sh
+++ scripts/v1_5/eval/docker_sqa.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-path="/localdisks/rogeralexis/robin_eval/playground"
+path="/app/playground"
 
-python /localdisks/rogeralexis/robin_eval/Robin/robin/eval/model_vqa_science.py \
+python /app/robin/robin/eval/model_vqa_science.py \
     --model-path $1 \
     --question-file  $path/data/eval/scienceqa/llava_test_CQM-A.json \
     --image-folder  $path/data/eval/scienceqa/images/test \
     --answers-file  $path/data/eval/scienceqa/answers/$3/llava-v1.5-13b.jsonl \
     --single-pred-prompt \
     --temperature 0 \
-    --conv-mode v1 \
+    --conv-mode vicuna_v1 \
     --model-base $2
 
 
-python /localdisks/rogeralexis/robin_eval/Robin/robin/eval/eval_science_qa.py \
+python /app/robin/robin/eval/eval_science_qa.py \
     --base-dir $path/data/eval/scienceqa/ \
     --result-file $path/data/eval/scienceqa/answers/$3/llava-v1.5-13b.jsonl \
     --output-file $path/data/eval/scienceqa/$3/llava-v1.5-13b_output.jsonl \
     --output-result $path/data/eval/scienceqa/answers/$3/llava-v1.5-13b_result.json
+
+model_name=$(basename $1)
+mkdir -p /export/$model_name/scienceqa
+mv $path/data/eval/scienceqa/answers/$3/llava-v1.5-13b.jsonl /export/$model_name/scienceqa/llava-v1.5-13b.jsonl
+mv $path/data/eval/scienceqa/$3/llava-v1.5-13b_output.jsonl /export/$model_name/scienceqa/llava-v1.5-13b_output.jsonl
+mv $path/data/eval/scienceqa/answers/$3/llava-v1.5-13b_result.json /export/$model_name/scienceqa/llava-v1.5-13b_result.json
