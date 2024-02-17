d04ac4c3ae084adba36327556e0ec1ed82434553
Alexis-BX <rogeralexis@icewindale.dms.umontreal.ca>
2024-02-17 10:53:35 -0500

    fixed gqa model name in docker script

--- scripts/v1_5/eval/docker_gqa.sh
+++ scripts/v1_5/eval/docker_gqa.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-MODEL=$1
+MODEL_PATH=$1
+MODEL_NAME=$(basename $1)
 BASE=$2
 #$3=""
 
@@ -8,23 +9,22 @@ SPLIT="llava_gqa_testdev_balanced"
 GQADIR="/app/playground/data/eval/gqa"
 
 python /app/robin/robin/eval/model_vqa_loader.py \
-    --model-path $MODEL \
+    --model-path $MODEL_PATH \
     --question-file $GQADIR/data/$SPLIT.jsonl \
     --image-folder $GQADIR/data/images \
-    --answers-file $GQADIR/answers/$3/$SPLIT/$MODEL/answers.jsonl \
+    --answers-file $GQADIR/answers/$3/$SPLIT/$MODEL_NAME/answers.jsonl \
     --temperature 0 \
     --model-base $BASE \
     --conv-mode vicuna_v1
 
-model_name=$(basename $1)
-mkdir -p /export/$model_name/gqa
-mv $GQADIR/answers/$3/$SPLIT/$MODEL/answers.jsonl /export/$model_name/gqa/answers.jsonl
+mkdir -p /export/$MODEL_NAME/gqa
+mv $GQADIR/answers/$3/$SPLIT/$MODEL_NAME/answers.jsonl /export/$MODEL_NAME/gqa/answers.jsonl
 
-output_file=$GQADIR/answers/$3/$SPLIT/$MODEL/answers.jsonl
+output_file=$GQADIR/answers/$3/$SPLIT/$MODEL_NAME/answers.jsonl
 
 python /app/robin/scripts/convert_gqa_for_eval.py --src $output_file --dst $GQADIR/data/testdev_balanced_predictions.json
 
-mv $GQADIR/data/testdev_balanced_predictions.json /export/$model_name/gqa/testdev_balanced_predictions.json
+mv $GQADIR/data/testdev_balanced_predictions.json /export/$MODEL_NAME/gqa/testdev_balanced_predictions.json
 
 cd $GQADIR
 python eval.py --tier data/testdev_balanced
