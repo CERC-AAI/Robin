1ac7c69799e073aa7ae8d228288d1f1ff78697d1
Alexis-BX <rogeralexis@icewindale.dms.umontreal.ca>
2024-02-18 16:12:53 -0500

    load_pretrained_model base none support

--- robin/model/builder.py
+++ robin/model/builder.py
@@ -28,55 +28,64 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
     if 'lora' in model_name.lower() and model_base is None:
         warnings.warn('There is `lora` in model name but no `model_base` is provided. If you are loading a LoRA model, please provide the `model_base` argument. Detailed instruction: https://github.com/haotian-liu/LLaVA#launch-a-model-worker-lora-weights-unmerged.')
         
-    lora_cfg_pretrained = AutoConfig.from_pretrained(model_path)
-    tokenizer = AutoTokenizer.from_pretrained(model_base)
-    print('Loading LLaVA from base model...')
-    
-    if 'mistral' in model_name.lower():
-        model = LlavaMistralForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=lora_cfg_pretrained, **kwargs)
-    elif any(x in model_name.lower() for x in ['neox', 'pythia', 'hi-nolin']):
-        model = LlavaGPTNeoXForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=lora_cfg_pretrained, **kwargs)
-    else:
-        model = LlavaLlamaForCausalLM.from_pretrained(
-            model_base, 
-            low_cpu_mem_usage=True, 
-            config=lora_cfg_pretrained, 
-            **kwargs,
-            # use_flash_attention_2 = True,
-        )
-    
-    token_num, tokem_dim = model.lm_head.out_features, model.lm_head.in_features
-    if model.lm_head.weight.shape[0] != token_num:
-        model.lm_head.weight = torch.nn.Parameter(torch.empty(token_num, tokem_dim, device=model.device, dtype=model.dtype))
-        model.model.embed_tokens.weight = torch.nn.Parameter(torch.empty(token_num, tokem_dim, device=model.device, dtype=model.dtype))
-
-    print('Loading additional LLaVA weights...')
-    if os.path.exists(os.path.join(model_path, 'non_lora_trainables.bin')):
-        print("Found non_lora_trainables")
-        non_lora_trainables = torch.load(os.path.join(model_path, 'non_lora_trainables.bin'), map_location='cpu')
-    else:
-        # this is probably from HF Hub
-        from huggingface_hub import hf_hub_download
-        def load_from_hf(repo_id, filename, subfolder=None):
-            cache_file = hf_hub_download(
-                repo_id=repo_id,
-                filename=filename,
-                subfolder=subfolder)
-            return torch.load(cache_file, map_location='cpu')
-        non_lora_trainables = load_from_hf(model_path, 'non_lora_trainables.bin')
-
-    non_lora_trainables = {(k[11:] if k.startswith('base_model.') else k): v for k, v in non_lora_trainables.items()}
-    if any(k.startswith('model.model.') for k in non_lora_trainables):
-        non_lora_trainables = {(k[6:] if k.startswith('model.') else k): v for k, v in non_lora_trainables.items()}
+    if model_base is not None:
+        lora_cfg_pretrained = AutoConfig.from_pretrained(model_path)
+        tokenizer = AutoTokenizer.from_pretrained(model_base)
+        print('Loading LLaVA from base model...')
         
-    model.load_state_dict(non_lora_trainables, strict=False)
+        if 'mistral' in model_name.lower():
+            model = LlavaMistralForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=lora_cfg_pretrained, **kwargs)
+        elif any(x in model_name.lower() for x in ['neox', 'pythia', 'hi-nolin']):
+            model = LlavaGPTNeoXForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=lora_cfg_pretrained, **kwargs)
+        else:
+            model = LlavaLlamaForCausalLM.from_pretrained(
+                model_base, 
+                low_cpu_mem_usage=True, 
+                config=lora_cfg_pretrained, 
+                **kwargs,
+                # use_flash_attention_2 = True,
+            )
+        
+        token_num, tokem_dim = model.lm_head.out_features, model.lm_head.in_features
+        if model.lm_head.weight.shape[0] != token_num:
+            model.lm_head.weight = torch.nn.Parameter(torch.empty(token_num, tokem_dim, device=model.device, dtype=model.dtype))
+            model.model.embed_tokens.weight = torch.nn.Parameter(torch.empty(token_num, tokem_dim, device=model.device, dtype=model.dtype))
 
-    from peft import PeftModel
-    print('Loading LoRA weights...')
-    model = PeftModel.from_pretrained(model, model_path)
-    print('Merging LoRA weights...')
-    model = model.merge_and_unload()
-    print('Model is loaded...')
+        print('Loading additional LLaVA weights...')
+        if os.path.exists(os.path.join(model_path, 'non_lora_trainables.bin')):
+            print("Found non_lora_trainables")
+            non_lora_trainables = torch.load(os.path.join(model_path, 'non_lora_trainables.bin'), map_location='cpu')
+        else:
+            # this is probably from HF Hub
+            from huggingface_hub import hf_hub_download
+            def load_from_hf(repo_id, filename, subfolder=None):
+                cache_file = hf_hub_download(
+                    repo_id=repo_id,
+                    filename=filename,
+                    subfolder=subfolder)
+                return torch.load(cache_file, map_location='cpu')
+            non_lora_trainables = load_from_hf(model_path, 'non_lora_trainables.bin')
+
+        non_lora_trainables = {(k[11:] if k.startswith('base_model.') else k): v for k, v in non_lora_trainables.items()}
+        if any(k.startswith('model.model.') for k in non_lora_trainables):
+            non_lora_trainables = {(k[6:] if k.startswith('model.') else k): v for k, v in non_lora_trainables.items()}
+            
+        model.load_state_dict(non_lora_trainables, strict=False)
+
+        from peft import PeftModel
+        print('Loading LoRA weights...')
+        model = PeftModel.from_pretrained(model, model_path)
+        print('Merging LoRA weights...')
+        model = model.merge_and_unload()
+        print('Model is loaded...')
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        model = LlavaLlamaForCausalLM.from_pretrained(
+            model_path, 
+            low_cpu_mem_usage=True, 
+            **kwargs,
+            use_flash_attention_2 = True,
+        )
 
     image_processor = None
 
