7d221338edeed68f81697c5e6f77276ffbea0544
Alexis-BX <rogeralexis@icewindale.dms.umontreal.ca>
2024-02-19 17:54:02 -0500

    added text vqa
    Co-authored-by: daniel-z-kaplan <48258016+daniel-z-kaplan@users.noreply.github.com>

--- scripts/v1_5/eval/textvqa.sh
+++ scripts/v1_5/eval/textvqa.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-python -m llava.eval.model_vqa_loader \
+path="/localdisks/rogeralexis/robin_eval/playground"
+
+python -m robin.eval.model_vqa_loader \
     --model-path $1 \
-    --question-file /pfss/mlde/workspaces/mlde_wsp_Ramstedt_Mila/datasets/playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
-    --image-folder /pfss/mlde/workspaces/mlde_wsp_Ramstedt_Mila/datasets/playground/data/eval/textvqa/train_images \
-    --answers-file /pfss/mlde/workspaces/mlde_wsp_Ramstedt_Mila/datasets/playground/data/eval/textvqa/answers/$3/llava-v1.5-13b.jsonl \
+    --question-file $path/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
+    --image-folder $path/data/eval/textvqa/train_images \
+    --answers-file $path/data/eval/textvqa/answers/$3/llava-v1.5-13b.jsonl \
     --temperature 0 \
     --model-base $2 \
     --conv-mode vicuna_v1
 
-python -m llava.eval.eval_textvqa \
-    --annotation-file /pfss/mlde/workspaces/mlde_wsp_Ramstedt_Mila/datasets/playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
-    --result-file /pfss/mlde/workspaces/mlde_wsp_Ramstedt_Mila/datasets/playground/data/eval/textvqa/answers/$3/llava-v1.5-13b.jsonl
+python -m robin.eval.eval_textvqa \
+    --annotation-file $path/data/eval/textvqa/TextVQA_0.5.1_val.json \
+    --result-file $path/data/eval/textvqa/answers/$3/llava-v1.5-13b.jsonl
